5c6b09feaddbfb568f9aa6cc4987c991ae1d5a05
Nick Rotella <nick.rotella@ainstein.ai>
2019-01-13 22:38:23 -0600

    Cleaning up K79 demo related nodes
    Working to merge all data visualization nodes into one, also refactor
    how parameters are set in each. Currently compiles but the 3d data
    viz node has broken, need to debug.

--- src/RadarDataVizPointCloud.cpp
+++ src/RadarDataVizPointCloud.cpp
@@ -5,6 +5,7 @@ RadarDataVizPointCloud::RadarDataVizPointCloud( std::string data_topic, std::str
     vel_topic_( vel_topic ),
     pcl_topic_( pcl_topic ),
     listen_tf_( buffer_tf_ )
+    
 {
   pub_pcl_ = node_handle_.advertise<sensor_msgs::PointCloud2>( pcl_topic_, 10 );
   sub_radar_data_ = node_handle_.subscribe( data_topic_, 10,
@@ -15,7 +16,7 @@ RadarDataVizPointCloud::RadarDataVizPointCloud( std::string data_topic, std::str
 					   this );
 
   // Get parameters:
-  node_handle_.param( "rel_speed_thresh", rel_speed_thresh_, 1.0 );
+  node_handle_.param( "max_speed_thresh", max_speed_thresh_, 1.0 );
   node_handle_.param( "min_dist_thresh", min_dist_thresh_, 1.0 );
   node_handle_.param( "max_dist_thresh", max_dist_thresh_, 20.0 );
     
@@ -33,30 +34,58 @@ void RadarDataVizPointCloud::radarVelCallback( const geometry_msgs::Twist &msg )
 void RadarDataVizPointCloud::radarDataCallback( const radar_sensor_msgs::RadarData &msg )
 {
   // Get the data frame ID and look up the corresponding tf transform:
-  Eigen::Affine3d tf_sensor_to_world = tf2::transformToEigen(
-							     buffer_tf_.lookupTransform( "base_link", msg.header.frame_id, ros::Time( 0 ) ) );
+  Eigen::Affine3d tf_sensor_to_world =
+    tf2::transformToEigen(buffer_tf_.lookupTransform( "base_link", msg.header.frame_id, ros::Time( 0 ) ) );
   
-  // First delete, then populate the raw pcls:
+  // Clear the point cloud point vector:
   pcl_.clear();
+
+  // Iterate through raw targets and add them to the point cloud:
   for( auto it = msg.raw_targets.begin(); it != msg.raw_targets.end(); ++it )
     {
-      // Compute the relative speed of target to radar in radar sensor frame:
+      // If the radar world frame velocity is available from another source, use it for further processing:
       if( is_vel_available_ )
 	{
-	  Eigen::Vector3d meas_dir = Eigen::Vector3d( cos( ( M_PI / 180.0 ) * it->azimuth ) * cos( ( M_PI / 180.0 ) * it->elevation ),
-						      sin( ( M_PI / 180.0 ) * it->azimuth ) * cos( ( M_PI / 180.0 ) * it->elevation ),
-						      sin( ( M_PI / 180.0 ) * it->elevation ) );
+	  // Copy the original radar target for further processing:
+	  radar_sensor_msgs::RadarTarget t = *it;
+	  
+	  // Compute the velocity of the radar rotated into instantaneous radar frame:
+	  Eigen::Vector3d vel_radar = tf_sensor_to_world.linear().inverse() * vel_world_;
+	  
+	  // Use the radar velocity information and target relative speed to compute elevation:
+	  // s = n^{T} * R_{W}^{R} * ( v_{T}^{W} - v_{R}^{W} )
+	  // s = -n^{T} * R_{W}^{R} * v_{R}^{W} assuming v_{T}^{W} = 0 (static target)
+	  // s = [-cos(a) * cos(e), -sin(a) * cos(e), -sin(e)] * v_{R}^{R}
+	  // s = -v_{T,x}^{R} * cos(a) * cos(e)
+	  //     - v_{T,y}^{R} * sin(a) * cos(e)
+	  //	 - v_{T,z}^{R} * sin(e)
+	  // 
+	  t.elevation = ( 180.0 / M_PI ) * acos( t.speed / ( -vel_radar( 0 ) * cos( ( M_PI / 180.0 ) * t.azimuth )
+							     - vel_radar( 1 ) * sin( ( M_PI / 180.0 ) * t.azimuth ) ) ) - 90.0;
+	  
+	  // Compute the unit vector along the axis between sensor and target:
+	  // n = [cos(azi)*cos(elev), sin(azi)*cos(elev), sin(elev)]
+	  Eigen::Vector3d meas_dir = Eigen::Vector3d( cos( ( M_PI / 180.0 ) * t.azimuth ) * cos( ( M_PI / 180.0 ) * t.elevation ),
+						      sin( ( M_PI / 180.0 ) * t.azimuth ) * cos( ( M_PI / 180.0 ) * t.elevation ),
+						      sin( ( M_PI / 180.0 ) * t.elevation ) );
 
-	  double rel_speed = it->speed - meas_dir.dot( tf_sensor_to_world.linear().inverse() * vel_world_ );
-	  // Filter out targets based on relative speed:
-	  if( std::abs( rel_speed ) < rel_speed_thresh_ &&
-	      it->range >= min_dist_thresh_ &&
-	      it->range <= max_dist_thresh_) // hack to filter close and far targets
+	  // Radar measures relative speed s of target along meas_dir.:
+	  // s = n^{T} * R^{T} * ( v_{T} - v_{car} )
+	  // We wish to filter out targets with nonzero world frame
+	  // velocity v_{T}, but can only compute the projection of v_{T}:
+	  // v_{T,proj} = n^{T} * R^{T} * v_{T} = s + n^{T} * R^{T} * v_{car}
+	  double proj_speed = t.speed - meas_dir.dot( tf_sensor_to_world.linear().inverse() * vel_world_ );
+	  
+	  // Filter out targets based on project target absolute speed
+	  // and range limits:
+	  if( std::abs( proj_speed ) < max_speed_thresh_ &&
+	      t.range >= min_dist_thresh_ &&
+	      t.range <= max_dist_thresh_)
 	    {
-	      pcl_.points.push_back( radarDataToPclPoint( *it ) );
+	      pcl_.points.push_back( radarDataToPclPoint( t ) );
 	    }
 	}
-      else // do not filter
+      else // No velocity information available
 	{
 	  pcl_.points.push_back( radarDataToPclPoint( *it ) );
 	}
