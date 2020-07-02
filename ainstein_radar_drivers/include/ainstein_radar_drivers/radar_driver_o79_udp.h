bb08aa22a2d6fa1a3e3d3f6bce06259febcbe9d6
Nick Rotella <nick.rotella@ainstein.ai>
2020-07-02 17:01:11 -0500

    Add O79 UDP driver, interface, node, and launch
    Added an O79 UDP radar driver (ROS independent), radar interface (ROS
    dependent), and node. This is very similar to the K79 node which also
    uses UDP, however it is updated to reflect new additions to the O79
    firmware including message headers and multiple message IDs.
    
    Also updated the existing O79 node launch file to use this new driver
    instead of the old K79 driver (which is no longer compatible with O79).

---
+++ ainstein_radar_drivers/include/ainstein_radar_drivers/radar_driver_o79_udp.h
@@ -0,0 +1,54 @@
+#ifndef RADAR_DRIVER_O79_UDP_H_
+#define RADAR_DRIVER_O79_UDP_H_
+
+#include <netinet/in.h>
+#include <string>
+#include <memory>
+#include <vector>
+
+#include "radar_target.h"
+
+namespace ainstein_radar_drivers
+{
+  
+  class RadarDriverO79UDP {
+
+  public:
+    RadarDriverO79UDP( std::string host_ip_address, int host_port,
+		    std::string radar_ip_address, int radar_port );         
+    ~RadarDriverO79UDP( void );
+
+    bool connect( void );
+    bool receiveTargets( std::vector<ainstein_radar_drivers::RadarTarget> &targets,
+			 std::vector<ainstein_radar_drivers::RadarTarget> &targets_tracked );
+  
+    static const std::string connect_cmd_str;
+    static const unsigned int connect_res_len;
+
+    static const std::string run_cmd_str;
+
+    static const unsigned int max_msg_len;
+    static const unsigned int msg_len_raw_targets;
+    static const unsigned int msg_len_tracked_targets;
+
+    static const unsigned int msg_header_len;
+
+    static const unsigned int msg_id_raw_targets;
+    static const unsigned int msg_id_tracked_targets;
+    
+  private:
+    std::string host_ip_addr_;
+    int host_port_;
+    std::string radar_ip_addr_;
+    int radar_port_;
+
+    int sockfd_; // socket file descriptor
+    struct sockaddr_in sockaddr_;
+    char* buffer_;
+
+    struct sockaddr_in destaddr_;
+  };
+
+} // namespace ainstein_radar_drivers
+
+#endif // RADAR_DRIVER_O79_H_
---
+++ ainstein_radar_drivers/src/radar_interface_o79_udp.cpp
@@ -0,0 +1,195 @@
+/*
+  Copyright <2018-2020> <Ainstein, Inc.>
+
+  Redistribution and use in source and binary forms, with or without modification, are permitted 
+  provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this list of 
+  conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of 
+  conditions and the following disclaimer in the documentation and/or other materials provided 
+  with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its contributors may be used to 
+  endorse or promote products derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
+  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <iostream>
+#include <cstdint>
+#include <cerrno>
+
+#include "ainstein_radar_drivers/radar_interface_o79_udp.h"
+
+namespace ainstein_radar_drivers
+{
+
+RadarInterfaceO79UDP::RadarInterfaceO79UDP( ros::NodeHandle node_handle,
+					    ros::NodeHandle node_handle_private ) :
+  nh_( node_handle ),
+  nh_private_( node_handle_private ),
+  radar_data_msg_ptr_raw_( new ainstein_radar_msgs::RadarTargetArray ),
+  radar_data_msg_ptr_tracked_( new ainstein_radar_msgs::RadarTargetArray ),
+  radar_info_msg_ptr_( new ainstein_radar_msgs::RadarInfo )
+{
+  // Get the host IP and port:
+  std::string host_ip_addr;
+  nh_private_.param( "host_ip", host_ip_addr, std::string( "10.0.0.75" ) );
+
+  int host_port;
+  nh_private_.param( "host_port", host_port, 1024 );
+
+  // Get the radar IP and port:
+  std::string radar_ip_addr;
+  nh_private_.param( "radar_ip", radar_ip_addr, std::string( "10.0.0.10" ) );
+
+  int radar_port;  
+  nh_private_.param( "radar_port", radar_port, 7 );
+
+  // Get the radar data frame ID:
+  nh_private_.param( "frame_id", frame_id_, std::string( "map" ) );
+
+  // Set the frame ID:
+  radar_data_msg_ptr_raw_->header.frame_id = frame_id_;
+  radar_data_msg_ptr_tracked_->header.frame_id = frame_id_;
+
+  // Publish the RadarInfo message:
+  publishRadarInfo();
+  
+  // Create the radar driver object:
+  driver_.reset( new RadarDriverO79UDP( host_ip_addr, host_port,
+					radar_ip_addr, radar_port ) );
+  
+  // Start the data collection thread:
+  thread_ = std::unique_ptr<std::thread>( new std::thread( &RadarInterfaceO79UDP::mainLoop, this ) );
+  mutex_.lock();
+  is_running_ = true;
+  mutex_.unlock();
+
+  // Advertise the O79 raw targets data:
+  pub_radar_data_raw_ = nh_private_.advertise<ainstein_radar_msgs::RadarTargetArray>( "targets/raw", 10 );
+
+  // Advertise the O79 tracked targets data:
+  pub_radar_data_tracked_ = nh_private_.advertise<ainstein_radar_msgs::RadarTargetArray>( "targets/tracked", 10 );
+}
+
+RadarInterfaceO79UDP::~RadarInterfaceO79UDP(void)
+{
+  mutex_.lock();
+  is_running_ = false;
+  mutex_.unlock();
+  thread_->join();
+} 
+
+void RadarInterfaceO79UDP::mainLoop(void)
+{
+  // Connect to the radar:
+  driver_->connect();
+  
+  // Enter the main data receiving loop:
+  bool running = true;
+  std::vector<ainstein_radar_drivers::RadarTarget> targets_raw;
+  std::vector<ainstein_radar_drivers::RadarTarget> targets_tracked;
+  while( running && !ros::isShuttingDown() )
+    {
+      // Call to block until data has been received:
+      if( driver_->receiveTargets( targets_raw, targets_tracked ) == false )
+	{
+	  ROS_WARN_STREAM( "Failed to read data: " << std::strerror( errno ) << std::endl );
+	}
+      else
+	{
+	  if( targets_raw.size() > 0 )
+	    {
+	      // Fill in the raw RadarTargetArray message from the received targets:
+	      radar_data_msg_ptr_raw_->header.stamp = ros::Time::now();
+	      radar_data_msg_ptr_raw_->targets.clear();
+	      for( const auto &t : targets_raw )
+		{
+		  radar_data_msg_ptr_raw_->targets.push_back( targetToROSMsg( t ) );
+		}
+	      
+	      // Publish the raw target data:
+	      pub_radar_data_raw_.publish( radar_data_msg_ptr_raw_ );
+	    }
+
+	  if( targets_tracked.size() > 0 )
+	    {
+	      // Fill in the tracked RadarTargetArray message from the received targets:
+	      radar_data_msg_ptr_tracked_->header.stamp = ros::Time::now();
+	      radar_data_msg_ptr_tracked_->targets.clear();
+	      for( const auto &t : targets_tracked )
+		{
+		  radar_data_msg_ptr_tracked_->targets.push_back( targetToROSMsg( t ) );
+		}
+	  
+	      // Publish the tracked target data:
+	      pub_radar_data_tracked_.publish( radar_data_msg_ptr_tracked_ );
+	    }
+	} 
+
+      // Check whether the data loop should still be running:
+      mutex_.lock();
+      running = is_running_;
+      mutex_.unlock();  
+    }
+}
+
+  void RadarInterfaceO79UDP::publishRadarInfo( void )
+  {    
+    // Advertise the O79 sensor info (LATCHED):
+    pub_radar_info_ = nh_private_.advertise<ainstein_radar_msgs::RadarInfo>( "radar_info", 10, true );
+
+    // Form the RadarInfo message which is fixed for a given sensor:
+    radar_info_msg_ptr_->header.stamp = ros::Time::now();
+    radar_info_msg_ptr_->header.frame_id = frame_id_;
+
+    radar_info_msg_ptr_->update_rate = UPDATE_RATE;
+    radar_info_msg_ptr_->max_num_targets = MAX_NUM_TARGETS;
+  
+    radar_info_msg_ptr_->range_min = RANGE_MIN;
+    radar_info_msg_ptr_->range_max = RANGE_MAX;
+  
+    radar_info_msg_ptr_->speed_min = SPEED_MIN;
+    radar_info_msg_ptr_->speed_max = SPEED_MAX;
+
+    radar_info_msg_ptr_->azimuth_min = AZIMUTH_MIN;
+    radar_info_msg_ptr_->azimuth_max = AZIMUTH_MAX;
+
+    radar_info_msg_ptr_->elevation_min = ELEVATION_MIN;
+    radar_info_msg_ptr_->elevation_max = ELEVATION_MAX;
+
+    radar_info_msg_ptr_->range_resolution = RANGE_RES;
+    radar_info_msg_ptr_->range_accuracy = RANGE_ACC;
+  
+    radar_info_msg_ptr_->speed_resolution = SPEED_RES;
+    radar_info_msg_ptr_->speed_accuracy = SPEED_ACC;
+
+    radar_info_msg_ptr_->azimuth_resolution = AZIMUTH_RES;
+    radar_info_msg_ptr_->azimuth_accuracy = AZIMUTH_ACC;
+
+    radar_info_msg_ptr_->elevation_resolution = ELEVATION_RES;
+    radar_info_msg_ptr_->elevation_accuracy = ELEVATION_ACC;
+  
+    // Publish the RadarInfo message once since it's latched:
+    pub_radar_info_.publish( radar_info_msg_ptr_ );
+  }
+  
+} // namespace ainstein_radar_drivers
