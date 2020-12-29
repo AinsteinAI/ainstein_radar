322730540ab8824a8e376bd20d899e5978547ddf
daveochs <david.ochs@ainstein.ai>
2020-12-29 16:08:24 -0600

    Publish empty raw target CAN and UDP frames after a timeout
    The radar sends tracked targets at a fixed rate, but it only sends raw
    target when it detects them. We now publish empty raw frames when we
    recieve a tracked target message from the radar (even if it's empty)
    and a certain amount of time has passed. This clears the display of
    raw targets in rviz if none are present and the radar is running.

--- ainstein_radar_drivers/src/radar_interface_o79_udp.cpp
+++ ainstein_radar_drivers/src/radar_interface_o79_udp.cpp
@@ -1,26 +1,26 @@
 /*
   Copyright <2018-2020> <Ainstein, Inc.>
 
-  Redistribution and use in source and binary forms, with or without modification, are permitted 
+  Redistribution and use in source and binary forms, with or without modification, are permitted
   provided that the following conditions are met:
 
-  1. Redistributions of source code must retain the above copyright notice, this list of 
+  1. Redistributions of source code must retain the above copyright notice, this list of
   conditions and the following disclaimer.
 
-  2. Redistributions in binary form must reproduce the above copyright notice, this list of 
-  conditions and the following disclaimer in the documentation and/or other materials provided 
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of
+  conditions and the following disclaimer in the documentation and/or other materials provided
   with the distribution.
 
-  3. Neither the name of the copyright holder nor the names of its contributors may be used to 
+  3. Neither the name of the copyright holder nor the names of its contributors may be used to
   endorse or promote products derived from this software without specific prior written permission.
 
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
-  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
-  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
   OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
@@ -40,6 +40,8 @@
 
 namespace ainstein_radar_drivers
 {
+
+	const ros::Duration t_raw_timeout = ros::Duration(0.4);
 
 RadarInterfaceO79UDP::RadarInterfaceO79UDP( ros::NodeHandle node_handle,
 					    ros::NodeHandle node_handle_private ) :
@@ -65,16 +67,16 @@ RadarInterfaceO79UDP::RadarInterfaceO79UDP( ros::NodeHandle node_handle,
   std::string radar_ip_addr;
   nh_private_.param( "radar_ip", radar_ip_addr, std::string( "10.0.0.10" ) );
 
-  int radar_port;  
+  int radar_port;
   nh_private_.param( "radar_port", radar_port, 7 );
 
   // Get the radar data frame ID:
   nh_private_.param( "frame_id", frame_id_, std::string( "map" ) );
 
   // Get whether to publish ROS point cloud messages:
-  nh_private_.param( "publish_raw_cloud", publish_raw_cloud_, false );  
-  nh_private_.param( "publish_tracked_cloud", publish_tracked_cloud_, false );  
-  
+  nh_private_.param( "publish_raw_cloud", publish_raw_cloud_, false );
+  nh_private_.param( "publish_tracked_cloud", publish_tracked_cloud_, false );
+
   // Set the frame ID:
   radar_data_msg_ptr_raw_->header.frame_id = frame_id_;
   radar_data_msg_ptr_tracked_->header.frame_id = frame_id_;
@@ -84,7 +86,7 @@ RadarInterfaceO79UDP::RadarInterfaceO79UDP( ros::NodeHandle node_handle,
 
   // Publish the RadarInfo message:
   publishRadarInfo();
-  
+
   // Create the radar driver object:
   driver_.reset( new RadarDriverO79UDP( host_ip_addr, host_port,
 					radar_ip_addr, radar_port ) );
@@ -126,20 +128,20 @@ RadarInterfaceO79UDP::~RadarInterfaceO79UDP(void)
   is_running_ = false;
   mutex_.unlock();
   thread_->join();
-} 
+}
 
 void RadarInterfaceO79UDP::mainLoop(void)
 {
   // Connect to the radar:
   driver_->connect();
-  
+
   // Enter the main data receiving loop:
   bool running = true;
   std::vector<ainstein_radar_drivers::RadarTarget> targets_raw;
   std::vector<ainstein_radar_drivers::RadarTarget> targets_tracked;
   std::vector<ainstein_radar_drivers::BoundingBox> bounding_boxes;
   std::vector<ainstein_radar_drivers::RadarTargetCartesian> targets_tracked_cart;
-  
+
   while( running && !ros::isShuttingDown() )
     {
       // Call to block until data has been received:
@@ -196,7 +198,25 @@ void RadarInterfaceO79UDP::mainLoop(void)
 		  ainstein_radar_filters::data_conversions::radarTargetArrayToROSCloud( *radar_data_msg_ptr_tracked_, *cloud_msg_ptr_tracked_ );
 		  pub_cloud_tracked_.publish( cloud_msg_ptr_tracked_ );
 		}
-	
+
+		// Publish an empty raw frame if sufficient time has passed since a real one was received
+		// This clears the rviz display if no points are detected and the radar is running
+		if ( (ros::Time::now() - radar_data_msg_ptr_raw_->header.stamp ) > t_raw_timeout )
+		{
+			radar_data_msg_ptr_raw_->header.stamp = ros::Time::now();
+			radar_data_msg_ptr_raw_->targets.clear();
+			// Publish the raw target data:
+			pub_radar_data_raw_.publish( radar_data_msg_ptr_raw_ );
+
+			// Optionally publish raw detections as ROS point cloud:
+			if( publish_raw_cloud_ )
+			{
+				ainstein_radar_filters::data_conversions::radarTargetArrayToROSCloud( *radar_data_msg_ptr_raw_, *cloud_msg_ptr_raw_ );
+				pub_cloud_raw_.publish( cloud_msg_ptr_raw_ );
+			}
+
+		}
+
 	    }
 
 	  if( bounding_boxes.size() > 0 )
@@ -209,7 +229,7 @@ void RadarInterfaceO79UDP::mainLoop(void)
 		{
 		  msg_ptr_tracked_boxes_->boxes.push_back( boundingBoxToROSMsg( b, frame_id_ ) );
 		}
-	  
+
 	      // Publish the tracked target data:
 	      pub_bounding_boxes_.publish( msg_ptr_tracked_boxes_ );
 	    }
@@ -252,28 +272,28 @@ void RadarInterfaceO79UDP::mainLoop(void)
 		    }
 
 		  pose_eigen.linear() = rot_mat;
-	
+
 		  geometry_msgs::Pose pose_msg;
 		  pose_msg = tf2::toMsg( pose_eigen );
 
 		  msg_ptr_tracked_targets_cart_pose_->poses.push_back( pose_msg );
 		}
-	  
+
 	      // Publish the tracked Cartesian poses:
 	      pub_tracked_targets_cart_pose_.publish( msg_ptr_tracked_targets_cart_pose_ );
 	    }
 
-	} 
+	}
 
       // Check whether the data loop should still be running:
       mutex_.lock();
       running = is_running_;
-      mutex_.unlock();  
+      mutex_.unlock();
     }
 }
 
   void RadarInterfaceO79UDP::publishRadarInfo( void )
-  {    
+  {
     // Advertise the O79 sensor info (LATCHED):
     pub_radar_info_ = nh_private_.advertise<ainstein_radar_msgs::RadarInfo>( "radar_info", 10, true );
 
@@ -283,10 +303,10 @@ void RadarInterfaceO79UDP::mainLoop(void)
 
     radar_info_msg_ptr_->update_rate = UPDATE_RATE;
     radar_info_msg_ptr_->max_num_targets = MAX_NUM_TARGETS;
-  
+
     radar_info_msg_ptr_->range_min = RANGE_MIN;
     radar_info_msg_ptr_->range_max = RANGE_MAX;
-  
+
     radar_info_msg_ptr_->speed_min = SPEED_MIN;
     radar_info_msg_ptr_->speed_max = SPEED_MAX;
 
@@ -298,7 +318,7 @@ void RadarInterfaceO79UDP::mainLoop(void)
 
     radar_info_msg_ptr_->range_resolution = RANGE_RES;
     radar_info_msg_ptr_->range_accuracy = RANGE_ACC;
-  
+
     radar_info_msg_ptr_->speed_resolution = SPEED_RES;
     radar_info_msg_ptr_->speed_accuracy = SPEED_ACC;
 
@@ -307,9 +327,9 @@ void RadarInterfaceO79UDP::mainLoop(void)
 
     radar_info_msg_ptr_->elevation_resolution = ELEVATION_RES;
     radar_info_msg_ptr_->elevation_accuracy = ELEVATION_ACC;
-  
+
     // Publish the RadarInfo message once since it's latched:
     pub_radar_info_.publish( radar_info_msg_ptr_ );
   }
-  
+
 } // namespace ainstein_radar_drivers
